85e0942768d6202cd4017d2f2a556cea98435cfd
Juan Macias <juan.macias.luque@gmail.com>
2015-10-13 12:01:20 +0100

    - Add a printer utility to avoid excessive System.out.print pollution in the code.

---
+++ src/me/juanmacias/utils/Printer.java
@@ -0,0 +1,14 @@
+package me.juanmacias.utils;
+
+
+public class Printer {
+
+    public static void println(String s) {
+        System.out.println(s);
+    }
+
+    public static void print(String s) {
+        System.out.print(s);
+    }
+
+}
